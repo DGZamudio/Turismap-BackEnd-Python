55027435b74af867d02a299abc0ca58ff8390c48
DGZamudio <dmzamudio80@soy.sena.edu.co>
2024-08-24 20:21:24 -0500

    Variables de entorno y secret key prueba de sesion

--- database.py
+++ database.py
@@ -1,9 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 uri = os.getenv('DATABASE_URL')
 
